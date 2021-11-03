5daa22425a81eb6591e7aed8efac89726c9131d1
Erfunll <erfan_eskandari80@yahoo.com>
2021-11-03 15:42:31 +0330

    add my project

---
+++ test1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+	printf ("Im Erfan Eskandari\n");
+	printf ("Computer Programer");
+	printf ("\n Bye");
+	
+	return 0;
+}
