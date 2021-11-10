f38a6460b920ecc8f16b5f9e4899bac4ebdc025a
Erfunll <Erfunll>
2021-11-10 10:16:35 +0330

    matris1

---
+++ C1.c
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <stdio.h>
+ 
+using namespace std;
+ 
+int main()
+{
+int a[2][2] = {{1,2},
+	       {4,5}};
+cout<<a[2][2];
+ 
+    return 0;
+}
